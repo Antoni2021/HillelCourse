716c9afb7465d0cab2e80faadae6be41fa5e452d
Antoni2021 <antonio.bond2013@gmail.com>
2021-03-18 21:11:12 +0200

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("Hello my dear friend. It`s me, I write you from inside of your programm.");
+		System.out.println("Please, input some text");
+
+
+	}
+}
