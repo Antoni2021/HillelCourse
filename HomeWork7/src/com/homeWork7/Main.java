007e4d0cdec1f7e0abe6f943bc118376e25596dc
Antoni2021 <antonio.bond2013@gmail.com>
2021-04-09 23:16:28 +0300

    Maked HomeWork7
    Entering in Collections with StringCollection

--- HomeWork7/src/com/homeWork7/Main.java
+++ HomeWork7/src/com/homeWork7/Main.java
@@ -4,5 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        StringCollection str = new StringCollection(5);
+        StringCollection sc = new StringCollection();
+        System.out.println(str);
+        System.out.println(str.add(4, "System"));
+        System.out.println(str);
+        System.out.println(str.add("Magnum"));
+        System.out.println(str);
+        System.out.println(str.delete(2));
+        System.out.println(str);
+        System.out.println(str.delete("4"));
+        System.out.println(str);
+        System.out.println(str.get(2));
+        System.out.println("---------------");
+
+        System.out.println(sc);
+        System.out.println(sc.add(4, "System"));
+        System.out.println(sc);
+        System.out.println(sc.add("Magnum"));
+        System.out.println(sc);
+        System.out.println(sc.delete(2));
+        System.out.println(sc);
+        System.out.println(sc.delete("Magnum"));
+        System.out.println(sc);
+        System.out.println(sc.get(2));
+
     }
 }
--- HomeWork7/src/com/homeWork7/StringCollection.java
+++ HomeWork7/src/com/homeWork7/StringCollection.java
@@ -1,14 +1,109 @@
 package com.homeWork7;
 
-public class StringCollection {
+import java.util.Arrays;
+
+public class StringCollection implements Cloneable {
 
     String[] arr;
-    int count;
 
     public StringCollection(int length) {
         this.arr = new String[length];
         for (int i = 0; i < arr.length; i++){
             arr[i] = i + "";
         }
+    }
+    public StringCollection() {
+        this.arr = new String[10];
+    }
+
+    public boolean add(int index, String value){
+        String[] collection;
+        if (arr.length < index){
+            collection = new String[index + 1];
+        } else {
+            collection = new String[arr.length + 1];
+        }
+        for (int i = 0; i < collection.length; i++){
+            if (i < index & i < arr.length){
+                collection[i] = arr[i];
+            } else if (i == index){
+                collection[i] = value;
+            } else if (i > index & i <= arr.length ) {
+                collection[i] = arr[i - 1];
+            }
+        }
+        arr = collection.clone();
+        return true;
+    }
+
+    public boolean add(String value){
+        String[] collection = new String[arr.length + 1];
+
+        for (int i = 0; i < collection.length; i++){
+            if (i <  arr.length){
+                collection[i] = arr[i];
+            } else {
+                collection[i] = value;
+            }
+        }
+        arr = collection.clone();
+        return true;
+    }
+
+    public boolean delete(int index){
+        if (arr.length <= index) {
+            return false;
+        }
+        String[] collection = new String[arr.length - 1];
+        for (int i = 0; i < collection.length; i++){
+            if (i < index){
+                collection[i] = arr[i];
+            } else {
+                collection[i] = arr[i + 1];
+            }
+        }
+        arr = collection.clone();
+        return true;
+    }
+
+    public boolean delete(String value){
+        int numb = 0;
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] != null && arr[i].equals(value)){
+                numb = i;
+            }
+        }
+        if (numb == 0) {
+            return false;
+        }
+        String[] collection = new String[arr.length - 1];
+        for (int i = 0; i < collection.length; i++){
+            if (i < numb){
+                collection[i] = arr[i];
+            } else {
+                collection[i] = arr[i + 1];
+            }
+        }
+        arr = collection.clone();
+        return true;
+    }
+
+    public String get(int index){
+        if (arr.length <= index) {
+            return "There are no this index in the collection";
+        }
+        return arr[index];
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "StringCollection{" +
+                "arr=" + Arrays.toString(arr) +
+                '}';
     }
 }
