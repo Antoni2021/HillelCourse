e319b2a7516562cab93a7bbc1392e66179372698
Antoni2021 <antonio.bond2013@gmail.com>
2021-05-10 17:20:44 +0300

    junit version
    change junit version

--- HomeWork12/src/test/java/com/work/StringCollectionTest.java
+++ HomeWork12/src/test/java/com/work/StringCollectionTest.java
@@ -6,39 +6,39 @@ import org.junit.jupiter.api.Test;
 public class StringCollectionTest {
 
     @RepeatedTest(100)
-    public void addTest() {
+    public void testAdd() {
         Assertions.assertTrue(new StringCollection(0).add(7));
     }
 
     @Test
-    public void testAdd() {
+    public void testAddIndex() {
         Assertions.assertFalse(new StringCollection(4).add(17, "s"));
     }
 
     @Test
-    public void deleteTest() {
+    public void testDelete() {
         Assertions.assertFalse(new StringCollection(0).delete(7));
     }
 
     @Test
-    public void getTest() {
+    public void testGet() {
         Assertions.assertEquals("There is no that index", new StringCollection(0).get(18));
         Assertions.assertEquals("2", new StringCollection(3).get(2));
     }
 
     @Test
-    public void containTest() {
+    public void testContain() {
         Assertions.assertFalse(new StringCollection(0).contain(7));
     }
 
     @Test
-    public void clear() {
+    public void testClear() {
         Assertions.assertFalse(new StringCollection(0).clear());
         Assertions.assertTrue(new StringCollection(1).clear());
     }
 
     @Test
-    public void size() {
+    public void testSize() {
         Assertions.assertEquals(4, new StringCollection(4).size());
     }
 }
