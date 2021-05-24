d4588e4d9cda7233e7105400c62c1cd125215cb5
Antoni2021 <antonio.bond2013@gmail.com>
2021-05-24 19:18:49 +0300

    idea for game
    some realization

--- HomeWork15/src/main/java/com/work/Main.java
+++ HomeWork15/src/main/java/com/work/Main.java
@@ -1,8 +1,12 @@
 package com.work;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
 
+//        HashMap<String, Hand> init = new HashMap<String, Hand>();
+        
     }
 }
