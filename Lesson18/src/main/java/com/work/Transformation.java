f9b19e4379133568d82934c9ac1de23fb02d920d
Antoni2021 <antonio.bond2013@gmail.com>
2021-06-07 02:21:38 +0300

    last changes
    finding a gson

--- Lesson18/src/main/java/com/work/Transformation.java
+++ Lesson18/src/main/java/com/work/Transformation.java
@@ -3,33 +3,28 @@ package com.work;
 import com.google.gson.Gson;
 import org.yaml.snakeyaml.Yaml;
 import java.io.*;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class Transformation {
 
     public static void fromJson(File file) {
-
         Gson gson = new Gson();
         Yaml yaml = new Yaml();
         char s = 0;
-        try {
-            s = (char) new FileReader(file).read();
-        } catch (IOException e) {
-            e.printStackTrace();
+        try (FileReader fr = new FileReader(file)) {
+            s = (char) fr.read();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
         try (FileReader fr = new FileReader(file);
-             FileWriter fw = new FileWriter(GetFiles.getFolder() + File.separator + "someNameAnother.yaml")){
+             FileWriter fw = new FileWriter(file.toString().split("\\.json")[0] + ".yaml")){
             if (s == '{') {
                 Map map = gson.fromJson(fr, Map.class);
                 fw.write(yaml.dump(map));
-                System.out.println(map);
-
             } else {
                 Map[] map = gson.fromJson(fr, Map[].class);
                 fw.write(yaml.dump(map));
-                System.out.println(Arrays.toString(map));
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
@@ -40,14 +35,13 @@ public class Transformation {
     public static void fromYaml(File file) {
         Yaml yaml = new Yaml();
         Gson gson = new Gson();
-
         try (FileReader fr = new FileReader(file);
-             FileWriter fw = new FileWriter(GetFiles.getFolder() + File.separator + "someNameAnother.json")){
+             FileWriter fw = new FileWriter(file.toString().split("\\.yaml")[0] + ".json")){
             Map<String, Object> obj = yaml.load(fr);
             fw.write(gson.toJson(obj));
         } catch (ClassCastException er){
             try (FileReader fr = new FileReader(file);
-                 FileWriter fw = new FileWriter(GetFiles.getFolder() + File.separator + "someNameAnother.json", true)){
+                 FileWriter fw = new FileWriter(file.toString().split("\\.yaml")[0] + ".json", true)){
                 List<Map> list = yaml.load(fr);
                 fw.write(gson.toJson(list));
             } catch (IOException e) {
@@ -58,14 +52,4 @@ public class Transformation {
         }
         file.delete();
     }
-
-//    public static String readToString(String filePath) {
-//        StringBuilder contentBuilder = new StringBuilder();
-//        try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8)) {
-//            stream.forEach(s -> contentBuilder.append(s).append("\n"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return contentBuilder.toString();
-//    }
 }
